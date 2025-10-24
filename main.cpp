0ab257ba13fd3ed4d847419ab1156005798a6263
Ervin Marin <ervinmarin49@gmail.com>
2025-10-25 01:01:13 +0300

    Updated command parsing

--- main.cpp
+++ main.cpp
@@ -3,124 +3,121 @@
 
 #define debug(x) std::cout << "variabila " << #x << " este " << x << std::endl;
 
-class Project
-{
+class Project {
+private:
+    std::string *words;
+    int noOfWords;
+
 public:
-    void parse_command(std::string command)
-    {
-        std::string word;
+    Project(std::string *words, int noOfWords) {
+        this->words = words;
+        this->noOfWords = noOfWords;
+    }
 
-        //construim primul cuvant din comanda, in care fiecare caracter va fi pus lowercase, ca stringul final sa fie case-insensitive
-        int i;
-        for (i = 0; command[i] != ' ' && i < command.length(); i++)
-        {
-            word += tolower(command[i]);
-        }
-        debug(word);
-
-        if (i < command.length())
-        {
-            i++; //i acum se afla pe primul caracter de dupa primul spatiu
-        }
-        else
-        {
-            std::cout << "Command cannot contain only one word!" << std::endl;
-        }
-
-        if (word == "create")
-        {
-            std::string second_word;
-            while (command[i] != ' ' && i < command.length())
-            {
-                second_word += tolower(command[i]);
-                i++;
-            }
-            debug(second_word);
-            if (second_word == "table")
-            {
+    void parse_command() {
+        std::string firstWord = words[0];
+        if (firstWord == "create") {
+            std::string secondWord = words[1];
+            if (secondWord == "table") {
                 create_table();
-            }
-            else
-            {
+            } else if (secondWord == "index") {
                 create_index();
+            } else {
+                std::cout << "Wrong statement! You can create a table or an index.";
             }
-        }
-        else if (word == "drop")
-        {
-            std::string second_word;
-            while (command[i] != ' ' && i < command.length())
-            {
-                second_word += command[i];
-                i++;
-            }
-            if (second_word == "table" || second_word == "TABLE")
-            {
+        } else if (firstWord == "drop") {
+            std::string secondWord = words[1];
+            if (secondWord == "table") {
                 drop_table();
-            }
-            else
-            {
+            } else if (secondWord == "index") {
                 drop_index();
+            } else {
+                std::cout << "Wrong statement! You can drop a table or an index.";
             }
-        }
-        else if (word == "display")
-        {
+        } else if (firstWord == "display") {
             display_table();
-        }
-        else if (word == "insert")
-        {
+        } else if (firstWord == "insert") {
             create_table();
-        }
-        else if (word == "select")
-        {
+        } else if (firstWord == "select") {
             create_table();
-        }
-        else if (word == "delete")
-        {
-        }
-        else if (word == "update")
-        {
-        }
-        else
-        {
-            std::cout << word << "Command is wrong";
+        } else if (firstWord == "delete") {
+        } else if (firstWord == "update") {
+        } else {
+            std::cout << "Command is wrong";
         }
     }
 
-    void create_table()
-    {
-        std::cout << "Table created";
+
+    void create_table() {
+        if (words[2] == "if") {
+            if (words[3] != "not" || words[4] != "exists") {
+                std::cout << "Syntax error!";
+                return;
+            }
+            return;
+        }
+        std::cout << "Table created successfully.";
     }
 
-    void create_index()
-    {
+    void create_index() {
+        std::cout << "Index created successfully.";
     }
 
-    void drop_table()
-    {
+    void drop_table() {
+        std::cout << "Dropped table succesfully.";
     }
 
-    void drop_index()
-    {
+    void drop_index() {
+        std::cout << "Dropped index successfully.";
     }
 
-    void display_table()
-    {
+    void display_table() {
+        std::cout << "Dropped index successfully.";
     }
 
-    void update_table()
-    {
+    void update_table() {
+        std::cout << "Updated table successfully.";
     }
 
-    void delete_from()
-    {
+    void delete_from() {
     }
 };
 
-int main()
-{
-    std::string command;
-    std::cin >> command;
-    Project* project = new Project();
-    project->parse_command(command);
+
+std::string *read_command_from_console(int &n);
+
+int main() {
+    int noOfWords;
+    std::string *words = read_command_from_console(noOfWords);
+    auto *project = new Project(words, noOfWords);
+    project->parse_command();
+    delete project;
+    delete[] words;
     return 0;
+}
+
+std::string *read_command_from_console(int &n) {
+    std::string s;
+    int noOfWords = 0;
+    getline(std::cin, s);
+    //numaram cate spatii sunt in tot stringul
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ')
+            noOfWords++;
+    }
+    noOfWords++; //numarul de cuvinte e egal cu numarul de spatii + 1
+
+    auto *words = new std::string[noOfWords];
+    int i = 0, currentWordIndex = 0;
+    while (i < s.length()) {
+        if (s[i] == ' ') {
+            currentWordIndex++;
+        } else {
+            //adaugam caracterele direct lowercase, ca stringul sa fie case-insensitive
+            words[currentWordIndex] += tolower(s[i]);
+        }
+        i++;
+    }
+    n = noOfWords;
+    return words;
 }
